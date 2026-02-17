457f8c19ad354a8b4f1e1e718e6e43b388a8a0d3
modulodos22-afk <modulodos22@gmail.com>
2026-02-16 21:05:51 -0300

    Update simple_server.py

--- wsgiref/simple_server.py
+++ wsgiref/simple_server.py
@@ -1,5 +1,10 @@
 # Archivo falso para engañar a Android
+
 class WSGIServer:
+    pass
+
+# ESTA es la clase que faltaba y causaba el error:
+class WSGIRequestHandler:
     pass
 
 def make_server(*args, **kwargs):
